9b0b022628d19f847d6978ded2aa6d7680f4bce5
YOSTINA-dh <yostinadesta@gmail.com>
2021-05-18 15:50:18 +0000

    setup allauth

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -120,7 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation .NumericPasswordValidator',
     },
 ]
 
